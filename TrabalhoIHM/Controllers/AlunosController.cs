a7ff2736f8b1898ae164729a14a69e941ed54a8d
israel.jesus <israel.jesus@FULL-TEC-0064>
2019-08-23 18:00:39 -0300

    Altera a AlunosController, adicionando IOC

--- TrabalhoIHM/Controllers/AlunosController.cs
+++ TrabalhoIHM/Controllers/AlunosController.cs
@@ -29,9 +29,9 @@ namespace TrabalhoIHM.Controllers
         }
 
         // GET: Alunos/Details/5
-        public ActionResult Details(int id)
+        public async Task<ActionResult> Details(int id)
         {
-            var aluno = _alunos.GetById(id);
+            var aluno =  await _alunos.GetById(id);
             if (aluno == null)
             {
                 return HttpNotFound();
@@ -45,81 +45,72 @@ namespace TrabalhoIHM.Controllers
             return View();
         }
 
-        // POST: Alunos/Create
-        // Para se proteger de mais ataques, ative as propriedades específicas a que você quer se conectar. Para 
-        // obter mais detalhes, consulte https://go.microsoft.com/fwlink/?LinkId=317598.
+     //Post: Cria um novo registro de Aluno
         [HttpPost]
         [ValidateAntiForgeryToken]
         public ActionResult Create([Bind(Include = "Id,Nome,Endereco,Telefone,Email,Nascimento,Ativo")] Aluno aluno)
         {
             if (ModelState.IsValid)
             {
-                _alunos.Save(aluno);
+                 _alunos.Save(aluno);
                 return RedirectToAction("Index");
             }
 
             return View(aluno);
         }
 
-        // GET: Alunos/Edit/5
-        public ActionResult Edit(int id)
+        // GET: Alunos/Edit/id
+        public async Task<ActionResult> Edit(int id)
         {
-            var aluno = _alunos.GetById(id);
-            if (aluno == null)
+            var aluno = await _alunos.GetById(id);
+            if ( aluno == null)
             {
                 return HttpNotFound();
             }
             return View(aluno);
         }
 
-        // POST: Alunos/Edit/5
-        // Para se proteger de mais ataques, ative as propriedades específicas a que você quer se conectar. Para 
-        // obter mais detalhes, consulte https://go.microsoft.com/fwlink/?LinkId=317598.
+        // POST: Alunos/Edit/id
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Edit([Bind(Include = "Id,Nome,Endereco,Telefone,Email,Nascimento,Ativo")] Aluno aluno)
+        public async Task<ActionResult> Edit(Aluno alunos)
         {
             if (ModelState.IsValid)
             {
-                var alunos = _alunos.CustomFind(x => x.Id == aluno.Id);
-                _alunos.Save(aluno);
+                var alunoss = await _alunos.GetById(alunos.Id);
+                _alunos.Save(alunos);
                 return RedirectToAction("Index");
             }
-            return View(aluno);
+            return View(alunos);
         }
 
         // GET: Alunos/Delete/5
-        public ActionResult Delete(int id)
+        public async Task<ActionResult> Delete(int id)
         {
 
-            var alunaaa = _alunos.GetById(id);
-            _alunos.DeleteMany(alunaaa);
+            var aluno = await _alunos.GetById(id);
             if (aluno == null)
             {
                 return HttpNotFound();
             }
             
-            return View();
+            return View(aluno);
         }
 
         // POST: Alunos/Delete/5
         [HttpPost, ActionName("Delete")]
         [ValidateAntiForgeryToken]
-        public ActionResult DeleteConfirmed(int id)
+        public async Task<ActionResult> DeleteConfirmed(int id)
         {
-            Aluno aluno = db.Alunos.Find(id);
-            db.Alunos.Remove(aluno);
-            db.SaveChanges();
+            var aluno = await _alunos.GetById(id);
+            if (aluno == null)
+            {
+                return HttpNotFound();
+            }
+            _alunos.Delete(aluno);
+
             return RedirectToAction("Index");
         }
 
-        protected override void Dispose(bool disposing)
-        {
-            if (disposing)
-            {
-                db.Dispose();
-            }
-            base.Dispose(disposing);
-        }
     }
 }
